22aea03da82ce963f93401c8e0f080314bd1fc16
Artemas <artemasseng@gmail.com>
2015-08-29 22:10:14 +0100

    Familiar with Arbitrary Maps as Bean Properties.

--- src/main/java/com/artemas/spring/test/Jungle.java
+++ src/main/java/com/artemas/spring/test/Jungle.java
@@ -9,9 +9,14 @@ public class Jungle {
 	
 	//Instantiate Map foods as a HashMap Object.
 	private Map<String, String> foods = new HashMap<String, String>();
+	private Map<String, Animal> animals = new HashMap<String, Animal>();
 	
 	public void setFoods(Map<String, String> foods){
 		this.foods = foods;
+	}
+	
+	public void setAnimals(Map<String, Animal> animals){
+		this.animals = animals;
 	}
 	
 	@Override
@@ -20,6 +25,11 @@ public class Jungle {
 		StringBuilder sb = new StringBuilder();
 		//for each entry in foods, get the key and get the value...
 		for(Map.Entry<String, String> entry: foods.entrySet()){
+			sb.append(entry.getKey() + ": " + entry.getValue() + "\n");
+		}
+		
+		//for each entry in animals, get the key and get the Animal...
+		for(Map.Entry<String, Animal> entry: animals.entrySet()){
 			sb.append(entry.getKey() + ": " + entry.getValue() + "\n");
 		}
 		
