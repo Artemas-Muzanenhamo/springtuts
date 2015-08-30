4ece21a909c5d7cf778ec5cd66753cad954c431e
Artemas <artemasseng@gmail.com>
2015-08-30 21:44:42 +0100

    JAV-87 - Familiar with implementation of the DAO

---
+++ src/main/java/com/artemas/spring/DAO/OffersDAO.java
@@ -0,0 +1,12 @@
+package com.artemas.spring.DAO;
+
+import java.util.List;
+
+import com.artemas.spring.model.Offer;
+
+public class OffersDAO {
+	
+	public List<Offer> getOffers(){
+		return null;
+	}
+}
